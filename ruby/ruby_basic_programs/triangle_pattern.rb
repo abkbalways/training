da8369acd36de6ad3fb06f114ab48328b4f3b1d1
abkbalways <abkbalways@gmail.com>
2022-01-10 15:53:02 +0530

    Add program triangle_pattern

---
+++ ruby/ruby_basic_programs/triangle_pattern.rb
@@ -0,0 +1,18 @@
+n=8
+i=0
+for i in 0...n
+    j=0
+    k=0
+    for j in 0...(n-i)
+    print(" ")
+    j +=1
+    end
+    for k in 0...i
+    print("* ")
+    k +=1
+    end
+
+    i +=1
+    puts("\n")
+end
+    
