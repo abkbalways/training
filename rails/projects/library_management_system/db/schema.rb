a33b3dff5bc4ca403759bf561c59e1d6f293f1d1
abkbalways <abkbalways@gmail.com>
2022-01-24 15:40:32 +0530

    Update latest schema of table books

--- rails/projects/library_management_system/db/schema.rb
+++ rails/projects/library_management_system/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_092656) do
+ActiveRecord::Schema.define(version: 2022_01_24_100609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,5 +26,16 @@ ActiveRecord::Schema.define(version: 2022_01_24_092656) do
     t.index ["isbn"], name: "books_isbn_key", unique: true
   end
 
-end
+  create_table "classes", force: :cascade do |t|
+    t.string "name"
+    t.string "class_id", null: false
+  end
 
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "class_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
