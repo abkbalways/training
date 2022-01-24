e0c83dc6c482c83937a5956fd5884b5269a05b57
abkbalways <abkbalways@gmail.com>
2022-01-24 16:01:02 +0530

    Update latest schema of project

--- rails/projects/library_management_system/db/schema.rb
+++ rails/projects/library_management_system/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_102057) do
+ActiveRecord::Schema.define(version: 2022_01_24_102654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2022_01_24_102057) do
     t.float "fine_per_day"
     t.integer "copies"
     t.index ["isbn"], name: "books_isbn_key", unique: true
+  end
+
+  create_table "borrows", force: :cascade do |t|
+    t.integer "isbn"
+    t.integer "student_id"
+    t.datetime "issue_date", precision: 6, null: false
+    t.datetime "submission_date", precision: 6, null: false
   end
 
   create_table "classes", force: :cascade do |t|
