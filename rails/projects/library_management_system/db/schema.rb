93ba454558d42c7679e2049501ed34c4ab967350
abkbalways <abkbalways@gmail.com>
2022-01-26 12:05:59 +0530

    Update schema of project

--- rails/projects/library_management_system/db/schema.rb
+++ rails/projects/library_management_system/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_25_074334) do
+ActiveRecord::Schema.define(version: 2022_01_26_062338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2022_01_25_074334) do
     t.integer "student_id"
     t.datetime "issue_date", precision: 6, null: false
     t.datetime "submission_date", precision: 6, null: false
+    t.boolean "accept"
   end
 
   create_table "sections", force: :cascade do |t|
