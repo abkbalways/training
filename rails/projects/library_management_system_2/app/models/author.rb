aa77087be7a077b9b7e44c837da25cd3c2685381
abkbalways <abkbalways@gmail.com>
2022-01-28 11:47:10 +0530

    Add Validations to author and book model

--- rails/projects/library_management_system_2/app/models/author.rb
+++ rails/projects/library_management_system_2/app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+  validates :name, presence: true, format: {with: /\A[A-Z a-z]+\z/ , message: "only letters allowed"}
+  validates :name, length: {maximum: 25}
+  validates :no_of_published_books, comparison: {less_than: 1000}, allow_nil: true
 end
--- rails/projects/library_management_system_2/app/models/book.rb
+++ rails/projects/library_management_system_2/app/models/book.rb
@@ -1,3 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :author
+  validates :id, comparison: {greater_than: 1000}, uniqueness: true
+  validates :name, presence: true, length: {maximum: 50}
+  validates :isbn, length: {is: 6}, uniqueness: true, allow_nil: true
+  validates :author_id, presence: true
+  validates :price, presence: true, numericality: true
+  validates :copies, presence: true, numericality: {only_integer: true}
 end
