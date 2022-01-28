cfe08da562cb07227fa343cd1b333e110b2b3f54
abkbalways <abkbalways@gmail.com>
2022-01-28 14:29:41 +0530

    Add has_one association between models 'head_of_section to section'

--- rails/projects/library_management_system_2/app/models/head_of_section.rb
+++ rails/projects/library_management_system_2/app/models/head_of_section.rb
@@ -1,6 +1,7 @@
 class HeadOfSection < ApplicationRecord
   validates :name, presence: true, format: {with: /\A[A-Z . a-z]+\z/ , message: "only letters allowed"}
   validates :name, length: {maximum: 25}
+  
   has_one :section
   
 end
--- rails/projects/library_management_system_2/app/models/section.rb
+++ rails/projects/library_management_system_2/app/models/section.rb
@@ -2,5 +2,5 @@ class Section < ApplicationRecord
   validates :name, presence: true
   validates :head_of_section_id, presence: true
   has_many :students
-  #belongs_to :head_of_section
+  belongs_to :head_of_section
 end
