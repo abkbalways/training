1dce3270b50bd032efc5f768000c7bc979bb48e9
abkbalways <abkbalways@gmail.com>
2022-02-01 12:27:49 +0530

    Add validation for email and mobile in 'students'

--- rails/projects/library_management_system_2/app/models/student.rb
+++ rails/projects/library_management_system_2/app/models/student.rb
@@ -3,7 +3,9 @@ class Student < ApplicationRecord
   validates :name, presence: true, length: {maximum: 25}
   validates :name, format: {with: /\A[A-Z a-z]+\z/}
   validates :city, format: {with: /\A[A-Z a-z]+\z/}, length: {maximum: 20}, allow_nil: true
-  validates :mobile, length: {is: 10}, presence: true, if: :section?
+  validates :mobile, presence: true, if: :section?
+  validates :mobile, length: {maximum: 10,minimum:10, too_long:"your number should not be greater than 10 digits", too_short:"Your number should not be lesser than 10 digits"}, allow_nil: true
+  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
   def section?
     section_id == 3 || section_id == 4
   end
@@ -12,9 +14,11 @@ class Student < ApplicationRecord
   #has_many :issues
   #has_many :books, through: :issues
   has_and_belongs_to_many :books
+
   after_validation :successfully_validation
   before_validation :remove_whitespaces
   after_create :display_student_age
+  
   
   def successfully_validation
     puts "You are validated successfully"
