7eb520bba85191d3fd1b78ba744761151cf4b282
abkbalways <abkbalways@gmail.com>
2022-02-01 14:55:38 +0530

    Add after_initialize for set id add by 1 after every book created

--- rails/projects/library_management_system_2/app/models/book.rb
+++ rails/projects/library_management_system_2/app/models/book.rb
@@ -6,13 +6,31 @@ class Book < ApplicationRecord
   validates :price, presence: true, numericality: true
   validates :copies, presence: true, numericality: {only_integer: true}
   belongs_to :author
-  #has_many :issues
-  #has_many :students, through: :issues
-  has_and_belongs_to_many :students, -> {where isbn:105698}
+  has_many :issues
+  has_many :students, through: :issues
+  #has_and_belongs_to_many :students, -> {where isbn:105698}
   has_many :feedbacks, as: :feedbackable
 
-  before_save :before_save_message
-  after_save :after_save_message
+  #before_save :before_save_message
+  #after_save :after_save_message
+  before_update :before_update_message
+  after_update :after_update_message
+  
+
+  after_initialize :default_values
+
+  private
+    def default_values
+      self.id ||= Book.last.id + 1
+    end
+  
+  def before_update_message
+    puts "Before isbn = #{isbn}"
+  end
+  def after_update_message
+   
+    puts "New isbn = #{isbn}"
+  end
 
   def before_save_message
     if self.author.present?
