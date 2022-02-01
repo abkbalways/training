2d022002fd40efc00b218ff7c7312b9895d9a890
abkbalways <abkbalways@gmail.com>
2022-02-01 14:23:09 +0530

    Add custom validation method in issues for check student has that book previous or not.

--- rails/projects/library_management_system_2/app/models/issue.rb
+++ rails/projects/library_management_system_2/app/models/issue.rb
@@ -3,4 +3,11 @@ class Issue < ApplicationRecord
   validates :student_id, presence: true
   belongs_to :book
   belongs_to :student
+  validate :issue_book
+
+  def issue_book
+    if Student.find(student_id).books.ids.include?(book_id)
+      errors.add(:book_id, 'Student has that book !')
+    end
+  end
 end
