4927a12dad878e89807e5ee41a21f0496a271cb9
abkbalways <abkbalways@gmail.com>
2022-02-02 15:24:35 +0530

    Add after_commit callback in 'author' model

--- rails/projects/library_management_system_2/app/models/author.rb
+++ rails/projects/library_management_system_2/app/models/author.rb
@@ -4,4 +4,8 @@ class Author < ApplicationRecord
   validates :no_of_published_books, comparison: {less_than: 1000}, allow_nil: true
   has_many :books
   has_many :feedbacks, as: :feedbackable
+  after_commit  :after_make
+  def after_make
+    puts "Hey It will added"
+  end
 end
