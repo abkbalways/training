76a1b8a66ec9344a0e16fd2b3d5cda6589ba2838
abkbalways <abkbalways@gmail.com>
2022-02-02 15:28:12 +0530

    Add before_destroy, after_desroy and after_update in model 'isssue'

--- rails/projects/library_management_system_2/app/models/issue.rb
+++ rails/projects/library_management_system_2/app/models/issue.rb
@@ -3,7 +3,26 @@ class Issue < ApplicationRecord
   validates :student_id, presence: true
   belongs_to :book
   belongs_to :student
-  validate :issue_book
+  validate :issue_book, on: :create
+  before_destroy :validating_return
+  after_destroy :thanks_you_message
+  after_update :count_book
+
+  def validating_return
+    if self.return_date == nil || ((self.return_date - self.issue_date)/(60*60*24)).floor > 15
+      errors.add(:base, "Can't be destroy")
+      puts "#{self.student.name} don't return book #{self.book.name } till now or #{self.student.name} has some fine !"
+      throw :abort
+    end
+  end
+
+  def thanks_you_message
+    puts "Thanks You #{self.student.name} for return book #{self.book.name} at before or on time !"
+  end
+
+  def count_book
+    puts "Now Remaining Books on you : #{self.student.books.count}"
+  end
 
   def issue_book
     if Student.find(student_id).books.ids.include?(book_id)
