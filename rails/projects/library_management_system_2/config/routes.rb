64110ff7c22b9a603988ba5fd3d8993b8d08dcf9
abkbalways <abkbalways@gmail.com>
2022-02-03 18:41:14 +0530

    Add routes for views of various controllers

--- rails/projects/library_management_system_2/config/routes.rb
+++ rails/projects/library_management_system_2/config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "front#index"
+  #  get "authors/:id" => "authors#author"
+   get "/authors" => "authors#author"
+   get "/sections" => "sections#section"
+   get "/authors/:id" => "books#book"
+   get "/sections/:id" => "students#student"
+   get "/books/:id" => "booksinfos#info"
+   get "/students/:id" => "studentinfos#info"
+
+   
 end
