d9a4c177b693392c4ee4e27bfdb14af9dd0d7454
abkbalways <abkbalways@gmail.com>
2022-02-04 18:15:46 +0530

    Add and change some actions in books controller

--- rails/projects/library_management_system_2/app/controllers/books_controller.rb
+++ rails/projects/library_management_system_2/app/controllers/books_controller.rb
@@ -1,5 +1,8 @@
 class BooksController < ApplicationController
   def book 
-    @books = Book.where(author_id:params[:id])
+    @books = Book.where(author_id:params[:aid])
+  end
+  def allbooks
+    @allbooks = Book.all
   end
 end
