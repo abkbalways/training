9970cdde37a2987133ac26005a14f30a698efe9d
abkbalways <abkbalways@gmail.com>
2022-02-04 18:16:20 +0530

    Add and change some actions in booksinfos controller controller

--- rails/projects/library_management_system_2/app/controllers/booksinfos_controller.rb
+++ rails/projects/library_management_system_2/app/controllers/booksinfos_controller.rb
@@ -1,6 +1,18 @@
 class BooksinfosController < ApplicationController
   def info
     @info = Book.find_by(id: params[:id])
+    @students = @info.issues
+    @available_copies = @info.copies - @students.where(return_date:nil).count
   end
+  def reviews
+    stars = 0
+    @info.feedbacks.each do |feedback|
+      {
+        stars => 5
+      }
+    end
+    return stars
+  end
+  helper_method :reviews
 
 end
