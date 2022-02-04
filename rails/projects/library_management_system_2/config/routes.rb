bf45ce737c93263afcee763f5835bc5b7ef5ac20
abkbalways <abkbalways@gmail.com>
2022-02-04 18:17:04 +0530

    Add some routes for links and for path

--- rails/projects/library_management_system_2/config/routes.rb
+++ rails/projects/library_management_system_2/config/routes.rb
@@ -6,10 +6,15 @@ Rails.application.routes.draw do
   #  get "authors/:id" => "authors#author"
    get "/authors" => "authors#author"
    get "/sections" => "sections#section"
-   get "/authors/:id" => "books#book"
+   get "/authors/:aid" => "books#book"
    get "/sections/:id" => "students#student"
    get "/books/:id" => "booksinfos#info"
    get "/students/:id" => "studentinfos#info"
-
+   get "/students/:id/authors" =>"authors#author"
+   get "/students/:id/authors/:aid" => "books#book"
+  #  get "/students/:id/books/:bid" => "Issues#issue"
+   get "/students/:id/books" => "books#allbooks"
+   get "/students/:id/books/:bid" => "addissues#after_issued"
+   get "/students/:id/books/:bid/return" => "addissues#return_book"
    
 end
