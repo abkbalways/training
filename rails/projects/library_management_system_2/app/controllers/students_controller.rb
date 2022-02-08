3166f77ea46692a9d02368ec6e33aae676629470
abkbalways <abkbalways@gmail.com>
2022-02-08 18:17:32 +0530

    Add update, edit, delete actions

--- rails/projects/library_management_system_2/app/controllers/students_controller.rb
+++ rails/projects/library_management_system_2/app/controllers/students_controller.rb
@@ -11,20 +11,38 @@ class StudentsController < ApplicationController
     @student = Student.find_by(id:params[:id])
     @age = Date.current.year - Student.find_by(id:params[:id]).dob.year
   end
+
   def new
-    
+    @student = Student.new 
   end
 
   def create
-    @student = Student.new(name:params[:name],city:params[:city],mobile:params[:mobile],section_id:params[:section_id],dob:params[:dob],email:params[:email])
-    @student.save
-    id = Student.last.id
-    redirect_to "/students/#{id}"
-    
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to "/students/#{@student.id}"
+    else
+      render "new" , status: :unprocessable_entity
+    end
   end
 
-  private
-    def student_params
-      params.require(:student).permit(:name, :city, :mobile, :section_id, :dob, :email)
-    end
+  def update
+    @student.update(student_params)
+    redirect_to "/students/#{@student.id}"
+  end
+
+  def edit
+    @student = Student.find_by(id:params[:id])
+  end
+
+  def delete
+    @student = Student.find_by(id:params[:id])
+    @student.destroy
+    redirect_to "/students"
+  end
+
+  private 
+  def student_params
+    params.require(:student).permit(:name, :section_id, :city, :mobile, :dob, :email)
+  end
+
 end
