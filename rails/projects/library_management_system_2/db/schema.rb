8efe51194e2b880a8f48202f03f7c0d2fccdaf9e
abkbalways <abkbalways@gmail.com>
2022-02-09 18:16:07 +0530

    Update the schema for add gender column in students table

--- rails/projects/library_management_system_2/db/schema.rb
+++ rails/projects/library_management_system_2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_093717) do
+ActiveRecord::Schema.define(version: 2022_02_09_061623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2022_02_01_093717) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
+    t.string "gender"
     t.index ["section_id"], name: "index_students_on_section_id"
   end
 
