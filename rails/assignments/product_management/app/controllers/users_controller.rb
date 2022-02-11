612bb4c0e40dc267182c4cebc709f5f48d28deed
abkbalways <abkbalways@gmail.com>
2022-02-11 16:09:22 +0530

    add logic for show the invalid user and password logic

--- rails/assignments/product_management/app/controllers/users_controller.rb
+++ rails/assignments/product_management/app/controllers/users_controller.rb
@@ -4,11 +4,14 @@ class UsersController < ApplicationController
     @username = params[:username]
     @password = params[:password]
     user = User.find_by(username:params[:username])
+    @id = 0
     if @username!="" and @username!=nil 
       if  user!=nil and user.username == @username and user.password == @password
         session[:user_id] = user.id
         redirect_to "/products"
+        @id = 0
       else
+        @id = 1
         render "login"
       end
     end
