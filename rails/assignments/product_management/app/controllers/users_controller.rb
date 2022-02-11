fb3fa944cfe74d12381ad8ac250de0ba7100c4b2
abkbalways <abkbalways@gmail.com>
2022-02-11 11:47:14 +0530

    Add controller for users actions

---
+++ rails/assignments/product_management/app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  
+  def login
+    @username = params[:username]
+    @password = params[:password]
+    user = User.find_by(username:params[:username])
+    @id = 0
+    if @username!="" and @username!=nil 
+      if  user!=nil and user.username == @username and user.password == @password
+        session[:user_id] = user.id
+        redirect_to "/users/new"
+        @id = 0
+      else
+        @id = 1
+        render "login"
+      end
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    # debugger
+    if @user.save 
+      redirect_to "/"
+    else 
+      render "new" , status: :unprocessable_entity
+    end
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:username, :password,:address,:dob, :password_confirmation, :terms)
+  end
+end
