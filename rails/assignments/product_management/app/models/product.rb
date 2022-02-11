45e9b0d14424866f6331409394de2d4dc41bf904
abkbalways <abkbalways@gmail.com>
2022-02-11 15:53:46 +0530

    Change name validation

--- rails/assignments/product_management/app/models/product.rb
+++ rails/assignments/product_management/app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  validates :name, presence: true,length: {maximum:15}, format: {with: /\A[A-Z0-9a-z]+\z/}
+  validates :name, presence: true,length: {maximum:15}, format: {with: /\A[A-Z 0-9a-z]+\z/}
   validates :price, presence: true, numericality: true, comparison: {less_than: 10000}
   belongs_to :user
 end
