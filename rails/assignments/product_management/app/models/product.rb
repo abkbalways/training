79d64e2d98fe96e46de9a4a19999803fe701c0c5
abkbalways <abkbalways@gmail.com>
2022-02-14 14:56:59 +0530

    Add callback for save product name in capitalize form

--- rails/assignments/product_management/app/models/product.rb
+++ rails/assignments/product_management/app/models/product.rb
@@ -1,5 +1,10 @@
 class Product < ApplicationRecord
   validates :name, presence: true,length: {maximum:15}, format: {with: /\A[A-Z 0-9a-z]+\z/}
-  validates :price, presence: true, numericality: true, comparison: {less_than: 10000}
+  validates :price, presence: true, numericality: true, comparison: {less_than: 100000}
+  before_validation :name_capitalize
   belongs_to :user
+
+  def name_capitalize
+    self.name = name.strip.capitalize
+  end
 end
