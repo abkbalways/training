8372ccff3384b09b4fa4128ed4c45c75427ceec7
abkbalways <abkbalways@gmail.com>
2022-02-15 16:35:45 +0530

    Add configuration of gmail and email id from which email will be sent

--- rails/assignments/product_management/config/environments/development.rb
+++ rails/assignments/product_management/config/environments/development.rb
@@ -67,4 +67,28 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_mailer.delivery_method = :sendmail
+# Defaults to:
+# config.action_mailer.sendmail_settings = {
+#   location: '/usr/sbin/sendmail',
+#   arguments: '-i'
+# }
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.default_options = {from: 'noreplyforme2@gmail.com'}
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+address:              'smtp.gmail.com',
+port:                 587,
+domain:               'google.com',
+user_name:            'noreplyforme2@gmail.com',
+password:             '9997817202',
+authentication:       'plain',
+enable_starttls_auto: true  }
+
+
+  
+
 end
