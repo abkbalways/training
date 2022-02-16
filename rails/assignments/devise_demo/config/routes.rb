1c54f796956ba4a5423c76c3f963b29f6f345510
abkbalways <abkbalways@gmail.com>
2022-02-16 16:53:37 +0530

    Update routes

--- rails/assignments/devise_demo/config/routes.rb
+++ rails/assignments/devise_demo/config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, 
+  controllers: {
+  sessions: 'users/sessions',
+  registrations: 'users/registrations',
+  passwords: 'users/passwords',
+  confirmations: 'users/confirmations'
+      
+
+  }
+
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
