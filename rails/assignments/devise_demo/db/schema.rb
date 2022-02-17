977b42acf1d286d3417a8453bc1a9fb1a54f2221
abkbalways <abkbalways@gmail.com>
2022-02-17 17:30:04 +0530

    Update Schema

--- rails/assignments/devise_demo/db/schema.rb
+++ rails/assignments/devise_demo/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_16_095532) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_16_124024) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,8 +31,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_16_095532) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "locked_at"
+    t.string "unlock_token"
+    t.integer "failed_attempts", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
 end
