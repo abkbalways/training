cc7b9a236c58c07d7c75af30adb53ade72317773
abkbalways <abkbalways@gmail.com>
2022-02-17 17:40:56 +0530

    Set my email for send mails

--- rails/assignments/devise_demo/config/environments/development.rb
+++ rails/assignments/devise_demo/config/environments/development.rb
@@ -70,7 +70,19 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.action_mailer.delivery_method = :letter_opener
+  
   config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.default_options = {from: 'noreplyforme2@gmail.com'}
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+address:              'smtp.gmail.com',
+port:                 587,
+domain:               'google.com',
+user_name:            'noreplyforme2@gmail.com',
+password:             '9997817202',
+authentication:       'plain',
+enable_starttls_auto: true  }
 
 end
